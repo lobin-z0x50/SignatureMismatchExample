c5cb47c2464c5ba353c822c87b6d11f9abd1a2cc
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-04-29 06:48:25 +0900

    feat: 最初の構成

---
+++ App/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lib;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- App started ---");
+
+            var c1 = new Class1();
+            c1.Print("Hello World!");
+        }
+    }
+}
---
+++ Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Lib
+{
+    public class Class1
+    {
+        public void Print(string message)
+        {
+            System.Console.Write("[Print] ");
+            System.Console.WriteLine(message);
+        }
+    }
+}
