feab1792da4161bde73409f210039b02e61b0893
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-04-29 06:50:04 +0900

    feat: Printメソッドにオプション域数を追加

--- Lib/Class1.cs
+++ Lib/Class1.cs
@@ -2,10 +2,15 @@
 {
     public class Class1
     {
-        public void Print(string message)
+        public void Print(string message, string? opt=null)
         {
-            System.Console.Write("[Print] ");
+            System.Console.Write("[Print] msg=");
             System.Console.WriteLine(message);
+            if (opt != null)
+            {
+                System.Console.Write("        opt=");
+                System.Console.WriteLine(opt);
+            }
         }
     }
 }
